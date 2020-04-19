ecbeecfa7293e9c3420c508ee7fc413028d864d4
martha-thomae <thomaemartha@gmail.com>
2020-04-19 15:00:12 -0400

    Add support for repeated tenor. There is still the issue that this is only copying the (repeated) children of the tenor layer, but it is not copying any elements contained within these. For example, if there is a ligature in the tenor layer within the sequence of notes to be repeated, the ligature will be copied but all the notes within that ligature won't (same with the lyrics of the notes).

--- Merge.js
+++ Merge.js
@@ -43,6 +43,89 @@ function clef_definition(staff, staffDef) {
     }
 }
 
+/*function get_set_of_repeated_notes(tenor_layer, startid, endid) {
+    const tenor_music_content = Array.from(tenor_layer.children);
+    
+    var repeating_tenor = Array.from([]);
+    var repeated_note_flag = false;
+
+    for (var noterest of tenor_music_content) {
+        // Find the first note on the repeating tenor (based on the startid)
+        // From this note the repeated_note_flag is set to true and it will remain
+        // as true until reaching the last note of the repeating tenor
+        if (noterest.getAttribute('xml:id') == startid) {
+            repeated_note_flag = true;
+        }
+        // All notes between the notes with startid and endid (inclusive), will be
+        // included into the repeating_tenor array (given the repeated_note_flag value)
+        if (repeated_note_flag) {
+            repeating_tenor.push(noterest);
+        }
+        // When finding the last repeated note (based on the endid)
+        // the repeated_note_flag will change back to false,
+        // so that no note after this one will be added into the repeating_tenor array
+        if (noterest.getAttribute('xml:id') == endid) {
+            repeated_note_flag = false;
+            break;
+        }
+    }
+    return repeating_tenor;
+}*/
+
+function get_set_of_repeated_notes(tenor_layer, startid, endid){
+    const tenor_music_content = Array.from(tenor_layer.children);
+    var start_index, end_index;
+    var index = 0;
+    // Find the indices representing the start and end of the repeated tenor
+    for (var noterest of tenor_music_content) {
+        if (noterest.getAttribute('xml:id') == startid) {
+            start_index = index;
+        } else if (noterest.getAttribute('xml:id') == endid) {
+            end_index = index;
+            break;
+        } index += 1;
+    }
+    // Use these indices to get the sequence of repeated notes/rests in the tenor
+    const repeated_notes = tenor_music_content.slice(start_index, end_index + 1);
+    return repeated_notes;
+}
+
+function expand_repeating_tenor(meiDoc){
+    // Find the <dir> element and use it to locate the tenor's <staff> and <layer> elements
+    const dir = meiDoc.getElementsByTagName('dir')[0];
+    const tenor_staff = dir.parentElement;
+    const tenor_layer = tenor_staff.getElementsByTagName('layer')[0];
+
+    // Retrieve <dir> attributes related to the repetition:
+    // - IDs pointing to the beginning and end of the set of notes to be repeated (@plist)
+    var plist_array = dir.getAttribute('plist').split(' ');
+    var startid_ref = plist_array[0];
+    var endid_ref = plist_array[1];
+    // - And the number of times this sequence of is to be repeated (encoded at @n for now)
+    var times = dir.getAttribute('n');
+
+    // Retrieve the notes repeated in the tenor
+    var repeating_tenor = get_set_of_repeated_notes(tenor_layer, startid_ref.slice(1,), endid_ref.slice(1,));
+
+    // Expand the repeated tenor into individual notes with @copyof
+    var xmlId, attribName, attribValue;
+    for (var i = 0; i < times; i++) {
+        for (var element of repeating_tenor){
+            var copied_element = meiDoc.createElementNS('http://www.music-encoding.org/ns/mei', element.tagName);
+            for (attribName in element.getAttributeNames) {
+                attribValue = element.getAttribute(attribName);
+                copied_element.setAttribute(attribName, attribValue);
+            }
+            xmlId = element.getAttribute('xml:id');
+            copied_element.setAttribute('copyof', '#'+xmlId);
+            copied_element.setAttribute('xml:id', xmlId+'_'+(i+1));
+            tenor_layer.appendChild(copied_element);
+        }
+    }
+    // Finally, remove the <dir> element encoding the repetition
+    tenor_staff.removeChild(dir);
+}
+
 const merge = meiDoc => {
     const mei = meiDoc.documentElement;
 
@@ -91,6 +174,10 @@ const merge = meiDoc => {
         // Remove unnecesary <clef> elements in the voice (i.e., <staff>) and
         // Encode inital clef within the voice's metadata (i.e., <staffDef> attributes)
         clef_definition(staff, staffDef);
+    }
+    // Expand repeating tenor if there is one
+    if (meiDoc.getElementsByTagName('dir').length != 0){
+        expand_repeating_tenor(meiDoc);
     }
 
     return meiDoc;
