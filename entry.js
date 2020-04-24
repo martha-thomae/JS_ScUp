2374433d7327d0ccde5f1523544cb6801a2870b5
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-04-24 16:08:56 -0400

    Export refine_score function

--- entry.js
+++ entry.js
@@ -1,8 +1,10 @@
 const MergeModule = require('./Merge.js');
 const ArsAntiqua = require('./ArsAntiqua.js');
+const PostProcessing = require('./postprocessing.js');
 // const ArsNova_and_WhiteMensural = require('./ArsNova_and_WhiteMensural.js');
 
 module.exports = {
   merge: MergeModule.merge,
   ArsAntiqua: ArsAntiqua,
+  refineScore: PostProcessing.refine_score,
 };
