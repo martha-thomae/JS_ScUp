83485ddfb3267852c8d3b2ddc24ff100af633a02
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-08-03 09:36:17 -0400

    Add ArsNova_and_WhiteMensural to exports

--- entry.js
+++ entry.js
@@ -1,10 +1,12 @@
 const MergeModule = require('./Merge.js');
 const ArsAntiqua = require('./ArsAntiqua.js');
+const ArsNova = require('./ArsNova_and_WhiteMensural.js');
 const PostProcessing = require('./postprocessing.js');
 // const ArsNova_and_WhiteMensural = require('./ArsNova_and_WhiteMensural.js');
 
 module.exports = {
   merge: MergeModule.merge,
   ArsAntiqua: ArsAntiqua,
+  ArsNova: ArsNova,
   refineScore: PostProcessing.refine_score,
 };
